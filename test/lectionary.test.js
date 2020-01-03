249f7b5d913824f4145c76957be92a3864c8a844
iethree <iethree@gmail.com>
2020-01-03 17:56:42 -0600

    fix collects

--- test/lectionary.test.js
+++ test/lectionary.test.js
@@ -1,27 +1,77 @@
-//lectionary test
-const lectionary = require('./lectionary');
+const rcl = require('../../daily-office');
+const lectionary = require('../src/lectionary.js')
+const time = require('../src/time.js');
 const log = require('logchalk');
-const datefns = require('date-fns');
 
-
-const NUM = 400;
-
-async function test(num){
-   var valid = 0;
-   for (let cnt = 0; cnt<num; cnt++){
-      let dt = datefns.addDays(new Date, cnt);
-      let r = await lectionary.psalms(dt).catch(e=>log.err(dt, e));
-      if(r)
-         valid++;
-   }
-   return valid;
+// getWeekTest();
+function getWeekTest(){
+   var [x,y] = lectionary.getWeek('20200101');
+   console.log(x, y)
 }
 
-test(NUM).then((valid)=>{
-   if(valid==NUM)
-      log.success("success: "+valid);
-   if(valid!==NUM)
-      log.err(`expected: ${NUM}, got ${valid}`);
-})
+lectionaryTest('20201231', 365);
+async function lectionaryTest(date, num){
+   let failures = 0;
+   for (let i=0; i<num; i++){
+      let result = await lectionary.getLectionary(date).catch(log.warn);
+      result = checkLectionary(result);
+      if(!result) failures++;
+      date = time.subDay(date);
+   }
+   if (failures>0) 
+      log.err('failures: ', failures, "/", num);
+   else 
+      log.success('no failures :-)');
+}
 
 
+function checkLectionary(l){
+   if(!l) return false;
+   var requirements = [
+      'collect'
+   ];
+
+   for(let i of requirements){
+      if(!l[i]) {
+         log.err(`missing property: ${i} in ${l.week}, ${l.day}`);
+         return false;
+      }
+   }
+   return true;
+}
+
+
+function translateTest(){
+   rcl.get({}).then((r)=>{
+      var seasons = [];
+      var weeks = [];
+      var shortWeeks = [];
+      var titles = [];
+   
+      for (let i of r){
+         if(seasons.indexOf(i.season)==-1)
+            seasons.push(i.season)
+         if(weeks.indexOf(i.week)===-1)
+            weeks.push(i.week)
+         if(i.title && titles.indexOf(i.title)===-1)
+            titles.push(i.title)
+      }
+   
+      for (let i in lectionary.calendar){
+         if(shortWeeks.indexOf(lectionary.calendar[i])===-1)
+            shortWeeks.push(lectionary.calendar[i]);
+      }
+   
+      //console.log('seasons', seasons.length);
+      for (let i in shortWeeks){
+         shortWeeks[i] = [shortWeeks[i], lectionary.getLongWeek(shortWeeks[i])];
+      }
+      console.table(shortWeeks)
+   
+      for (let i in weeks){
+         weeks[i] = [weeks[i], lectionary.getShortWeek(weeks[i])];
+      }
+      console.table(weeks)
+      //console.log('titles', titles.length);
+   })
+}
