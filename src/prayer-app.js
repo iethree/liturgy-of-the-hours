c4895edc339720507b739eaaac09e9c57eba00d1
Ryan Laurie <iethree@gmail.com>
2021-07-04 16:06:13 -0500

    error template

--- src/prayer-app.js
+++ src/prayer-app.js
@@ -41,7 +41,7 @@ dailyapp.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', err.message);
 });
 
 dailyapp.listen(PORT, ()=>console.log("prayer app on port "+PORT));
